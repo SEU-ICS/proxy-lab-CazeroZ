6f0decb9392b62c871d7c1bc9dc7803dc8b0ce2e
Cazero <2092073737@qq.com>
2024-08-22 16:37:49 +0800

    try

---
+++ cache.c
@@ -0,0 +1,76 @@
+#include "csapp.h"
+#include "cache.h"
+static cache_t cache;
+static sem_t mutex;w;
+
+static int readcnt,timestamp;
+
+
+void init_cache(){
+    cache.using_cache_num=0;
+    Sem_init(&mutex,0,1);
+    Sem_init(&w,0,1);
+    readcnt=0;
+    timestamp=0;
+}
+
+int query_cache(rio_t* rio_p, string url){
+    P(&mutex);
+    readcnt++;
+    if(readcnt==1){
+        P(&w);
+    }
+    V(&mutex);
+    int hit_flag=0;
+
+    for(int i=0;i<MAX_CACHE_NUM;i++){
+        if(strcmp(cache.cache_files[i].url,url)==0){
+            hit_flag=1;
+            P(&mutex);
+            cache.cache_files[i].timestamp=timestamp++;
+            V(&mutex);
+            rio_writen(rio_p->rio_fd,cache.cache_files[i].content,cache.cache_files[i].content_size);
+            hit_flag=1;
+            break;
+        }
+    }
+    P(&mutex);
+    readcnt--;
+    if(readcnt==0){
+        V(&w);
+    }
+    V(&mutex);
+    if(hit_flag)
+        return 1;
+    return 0;
+}
+int add_cache(string url,char* content, int content_size){
+    P(&w);
+    if(cache.using_cache_num==(MAX_CACHE_NUM)-1){
+        int oldest_index;
+        int oldest_timestamp=timestamp;
+        for(int i=0;i<cache.using_cache_num;i++){
+            if(cache.cache_files[i].timestamp<oldest_timestamp){
+                oldest_timestamp=cache.cache_files[i].timestamp;
+                oldest_index=i;
+            }
+        }
+        strcpy(cache.cache_files[oldest_index].url, url);
+        memcpy(cache.cache_files[oldest_index].content, content, content_size);
+        cache.cache_files[oldest_index].content_size=content_size;
+        P(&mutex);
+        cache.cache_files[oldest_index].timestamp=timestamp++;
+        V(&mutex);
+    }
+    else{
+        strcpy(cache.cache_files[cache.using_cache_num].url, url);
+        memcpy(cache.cache_files[cache.using_cache_num].content, content, content_size);
+        cache.cache_files[cache.using_cache_num].content_size=content_size;
+        P(&mutex);
+        cache.cache_files[cache.using_cache_num].timestamp=timestamp++;
+        cache.using_cache_num++;
+        V(&mutex);
+    }
+    V(&w);
+    return 0;
+}
--- proxy.c
+++ proxy.c
@@ -1,14 +1,182 @@
 #include <stdio.h>
-
+#include "csapp.h"
+#include "cache.h"
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 
+typedef char string[MAXLINE];
+typedef struct {
+    string hostname;
+    string port;
+    string path;
+} url_t;
+void* thread(void *vargp);
+void process(rio_t* client_rio_p,string url);
+int parse_url(string url, url_t* url_info);
+int parse_header(rio_t* client_rio_p, string header_info, string host);
+
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-int main()
-{
-    printf("%s", user_agent_hdr);
+int main(int argc,char **argv)
+{   
+    signal(SIGPIPE, SIG_IGN);
+    int listenfd, *connfdp;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    pthread_t tid;
+    if(argc != 2){
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+    listenfd = Open_listenfd(argv[1]);
+    init_cache();
+    while(1){
+        clientlen=sizeof(struct sockaddr_storage);
+        connfdp=(int*)malloc(sizeof(int));
+        *connfdp=Accept(listenfd,(SA*)&clientaddr,&clientlen);
+        if(*connfdp<0){
+            fprintf(stderr, "accept error: %s\n", strerror(errno));
+            continue;
+        }
+        Pthread_create(&tid,NULL,thread,connfdp);
+    }
+    close(listenfd);
     return 0;
+}
+void* thread(void *vargp){
+    pthread_detach(pthread_self());
+    int connfd=*((int*)vargp);
+    free(vargp);
+    rio_t client_rio;
+    string buf;
+    rio_readinitb(&client_rio,connfd);
+    if(rio_readlineb(&client_rio,buf,MAXLINE)<=0){
+        Close(connfd);
+        return NULL;
+    }
+    string method,url,http_version;
+    if(sscanf(buf,"%s %s %s",method,url,http_version)!=3){
+        close(connfd);
+        return NULL;
+    }
+    if(!strcasecmp(method,"GET")){
+        process(&client_rio,url);
+    }
+    close(connfd);
+    return NULL;
+}
+int parse_url(string url,url_t* url_info){
+    const int http_prefix_len=7;
+    if(strncasecmp(url,"http://",http_prefix_len)!=0){
+        fprintf(stderr, "url error, not http protocol: %s\n", url);
+        return -1;
+    }
+    char* host_ptr=url+http_prefix_len;
+    char* port_ptr=strchr(host_ptr,':');
+    char* path_ptr=strchr(host_ptr,'/');
+    if(path_ptr==NULL){
+        fprintf(stderr, "url error, no path: %s\n", url);
+        return -1;
+    }
+    if(port_ptr==NULL){
+        *path_ptr='\0';
+        strcpy(url_info->hostname,host_ptr);
+        strcpy(url_info->port,"80");
+        *path_ptr='/';
+        strcpy(url_info->path,path_ptr);
+    }
+    else{
+        *port_ptr='\0';
+        strcpy(url_info->hostname,host_ptr);
+        *port_ptr=':';
+        *path_ptr='\0';
+        strcpy(url_info->port,port_ptr+1);
+        *path_ptr='/';
+        strcpy(url_info->path,path_ptr);
+    }
+    return 0;
+}
+int parse_header(rio_t* client_rio_p, string header_info, string host){
+    string buf;
+    int has_host_flag=0;
+    while(1){
+        rio_readlineb(client_rio_p,buf,MAXLINE);
+        if(!strcmp(buf,"\r\n")){
+            break;
+        }
+        if(strncasecmp(buf,"Host:",5)==0){
+            has_host_flag=1;
+        }
+        if(strncasecmp(buf,"Connection:",strlen("Connection:"))==0){
+            continue;
+        }
+        if(strncasecmp(buf,"Proxy-Connection:",strlen("Proxy-Connection:"))==0){
+            continue;
+        }
+        if(strncasecmp(buf,"User-Agent:",11)==0){
+            continue;
+        }
+        strcat(header_info,buf);
+    }
+    if(!has_host_flag){
+        sprintf(buf,"Host: %s\r\n",host);
+        strcpy(header_info,buf);
+    }
+    strcat(header_info, "Connection: close\r\n");
+    strcat(header_info, "Proxy-Connection: close\r\n");
+    strcat(header_info, user_agent_hdr);
+    strcat(header_info, "\r\n");
+    return 0;
+}
+void process(rio_t* client_rio_p,string url){
+    if(query_cache(client_rio_p,url)){
+        return;
+    }
+    url_t url_info;
+    if(parse_url(url,&url_info)<0){
+        fprintf(stderr, "parse url error: %s\n", url);
+        return;
+    }
+    string header_info;
+    parse_header(client_rio_p,header_info,url_info.hostname);
+    int server_fd=open_clientfd(url_info.hostname,url_info.port);
+    if(server_fd<0){
+        fprintf(stderr, "open client fd error: %s\n", url);
+        return;
+    }
+    rio_t server_rio;
+    rio_readinitb(&server_rio,server_fd);
+    string buf;
+    sprintf(buf, "GET %s HTTP/1.0\r\n%s", url_info.path, header_info);
+     if (rio_writen(server_fd, buf, strlen(buf)) != strlen(buf)) {
+        fprintf(stderr, "Send request line and header error\n");
+        close(server_fd);
+        return;
+    }
+    int resp_total = 0, resp_current = 0;
+    char file_cache[MAX_OBJECT_SIZE];
+    int client_fd = client_rio_p->rio_fd;
+    while((resp_current=rio_readnb(&server_rio,file_cache,MAXLINE))){
+        if (resp_current < 0) {
+            fprintf(stderr, "Read server response error\n");
+            close(server_fd);
+            return;
+        }
+        if (resp_total + resp_current < MAX_OBJECT_SIZE) {
+            memcpy(file_cache + resp_total, buf, resp_current);
+        }
+        resp_total += resp_current;
+        if(rio_writen(client_fd,buf,resp_current)!=resp_current){
+            fprintf(stderr, "Send response to client error\n");
+            close(server_fd);
+            return;
+        }
+    }
+    if (resp_total < MAX_OBJECT_SIZE) {
+        add_cache(url, file_cache, resp_total);
+    }
+    close(server_fd);
+    return;
 }
